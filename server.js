d9ed403a9afca9c25ff504d51303ca7484df5b02
Lou-HTX <luismoreno.it@gmail.com>
2017-07-11 09:33:14 -0500

    db connection fixes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
 var db = require("./models");
 var sequelize = require("sequelize");
-var methodOverride = require("method-override");
 // ====================================================================
 // Serve static content for the app from the "public" directory in the application directory.
 // ====================================================================
@@ -44,10 +44,11 @@ db.sequelize.sync({ force: false }).then(function() {
 // ====================================================================
 //starting express
 // ====================================================================
-app.listen(port, function(err) {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    if (!err)
-        console.log("Site is live");
-    else
-        console.log(err);
-});
+// app.listen(port, function(err) {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//     if (!err)
+//         console.log("Site is live");
+//     else
+//         console.log(err);
+// });
+app.listen(port);
